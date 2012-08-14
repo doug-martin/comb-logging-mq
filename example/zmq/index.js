ef5d30b325d8aa8f3a62193f88016a2e09a0760c
Doug Martin <doug@dougamartin.com>
2012-08-14 16:00:22 -0500

    zeromq appender

---
+++ example/zmq/index.js
@@ -0,0 +1,44 @@
+/*
+ *
+ * Publisher subscriber pattern
+ *
+ */
+
+var cluster = require('cluster'),
+    zmq = require('zmq'),
+    address = 'tcp://127.0.0.1:12345',
+    ZeroMqAppender = require("../../index.js").ZeroMqAppender;
+comb = require("comb");
+
+if (cluster.isMaster) {
+    for (var i = 0; i < 2; i++) cluster.fork();
+
+    cluster.on('death', function (worker) {
+        console.log('worker ' + worker.pid + ' died');
+    });
+
+    var logger = comb.logging.Logger.getLogger("zmq-test");
+    logger.addAppender(new ZeroMqAppender({address:address}));
+    logger.addAppender(new ZeroMqAppender({name : "customEnvelope", address:address, envelope:"{levelName}-{name}"}));
+    var levels = ["debug", "trace", "info", "warn", "error", "fatal"], count = 0;
+    setInterval(function () {
+        var level = levels[count++ % levels.length];
+        logger[level]("logging a " + level.toUpperCase() + " message");
+    }, 500);
+    logger.info("starting");
+
+
+} else {
+    //subscriber = receive only
+    var socket = zmq.socket('sub');
+
+    socket.identity = 'subscriber' + process.pid;
+
+    socket.connect(address);
+    //socket.subscribe("INFO");
+    socket.subscribe("INFO-zmq-test");
+    socket.on('message', function (envelope, data) {
+        console.log(socket.identity + ': received \n\tenvelope: %s \n\tdata : %s', envelope.toString(), data.toString());
+    });
+
+}
